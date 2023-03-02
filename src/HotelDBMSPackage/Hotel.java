30cfa929f8d5ea9da31e57a0a241125bdfe56c4a
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-03-02 16:14:10 +0400

    adding singal row in mutiple tables together at the same time with only one input procces

--- src/HotelDBMSPackage/Hotel.java
+++ src/HotelDBMSPackage/Hotel.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 
 public class Hotel {
 	public static void main(String[] args) {
+		
+		System.out.println("running");
 //		    			        String url = "jdbc:sqlserver://localhost:1433;" +
 //		    			                "databaseName=HotelDBMS;" +
 //		    			                "encrypt=true;" +
@@ -100,6 +102,18 @@ public class Hotel {
 		String user = "sa";
 		String pass = "root";
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("//////////////////////////the first table Entring ///////////////////////////////////////");
+		System.out.println("Enter the hotel Name");
+		String hotel_name = scanner.next();
+		System.out.println("Enter the hotel location ");
+		String hotel_location = scanner.next();
+		System.out.println("Enter the created_date ");
+		String hotel_created_date = scanner.next();
+		System.out.println("Enter the update_date");
+		String hotel_update_date = scanner.next();
+		System.out.println("hotel is_Active");
+		String hotel_is_Active = scanner.next();
+		System.out.println("///////////////////////////the second table Entring ///////////////////////////////////////////");
 		System.out.println("enter id");
 		int id = scanner.nextInt();
 		System.out.println("enter the room type");
@@ -110,6 +124,28 @@ public class Hotel {
 		String updated_date = scanner.next();
 		System.out.println("enter is_Active");
 		String is_active = scanner.next();
+		System.out.println("///////////// the 3 table Entring  /////////////////");
+		System.out.println("Enter the guest ID");
+		int guest_id = scanner.nextInt();
+		System.out.println("Enter the guest name");
+		String guest_name = scanner.next();
+		System.out.println("Enter the guest phone");
+		String guest_phone = scanner.next();
+		System.out.println("Enter guest_accompanying_members");
+		int guest_accompanying_members = scanner.nextInt();
+		System.out.println("Enter paument amount");
+		String guest_payment_amount = scanner.next();
+		System.out.println("Enter the guest room ID");
+		int room_id= scanner.nextInt();
+		System.out.println("Enter hotel ID");
+		int hotel_IID = scanner.nextInt();
+		System.out.println("Enter created_date");
+		String guest_created_date = scanner.next();
+		System.out.println("Enter updated_date");
+		String guets_updated_date = scanner.next();
+		System.out.println("Enter is Active");
+		String guest_is_active = scanner.next();
+		
 		Connection con = null;
 		try {
 			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
@@ -118,11 +154,20 @@ public class Hotel {
 			Statement st = con.createStatement();
 
 			// String sql = "insert into Table_1 values('" + name+ "'," + roll + ",'" + cls+ "')";
-//			String sql = "INSERT INTO Hotels ('" + id + "'," + hotel_name + "'," + hotel_location + "'," + created_date
-//					+ updated_date + "'," + is_Active + "')";
-			String sql = "INSERT INTO Employee_Type (id, employee_type_name, created_date, updated_date, is_active) " +
-                    "VALUES ('" + id + "','" + room_type + "','" + created_date +"','" + updated_date + "','" + is_active + "')";
-
+           //String sql = "INSERT INTO Hotels ('" + id + "'," + hotel_name + "'," + hotel_location + "'," + created_date
+           //+ updated_date + "'," + is_Active + "')";
+			String sql = "INSERT INTO Hotels (hotel_name,hotel_location,created_date,updated_date,is_Active)"+
+		            "VALUES ("+"'"+hotel_name+"','"+hotel_location+"','"+hotel_created_date+"','"+hotel_update_date+ "',"+hotel_is_Active+");"
+					
+		    +"INSERT INTO Employee_Type (id, employee_type_name, created_date, updated_date, is_active) " +
+            "VALUES ('" + id + "','" + room_type + "','" + created_date +"','" + updated_date + "','" + is_active + "');"
+                    
+                    
+			+"INSERT INTO Guests (id, guest_name, guest_phone, guest_accompanying_members, guest_payment_amount,room_id,"
+			+ "hotel_id,created_date,updated_date,is_active) " +
+            "VALUES (" + guest_id + ",'" + guest_name + "','" + guest_phone +"'," + guest_accompanying_members + ",'"
+			+ guest_payment_amount +"',"+ room_id +"," +hotel_IID+",'"+guest_created_date+ "','"+guets_updated_date+ "',"+guest_is_active+ ")";
+			System.out.println(sql);
 
 			Integer m = st.executeUpdate(sql);
 			if (m >= 1) {
@@ -133,16 +178,18 @@ public class Hotel {
 			String sql1 = "Select * from Employee_Type";
 			ResultSet resultSet = st.executeQuery(sql1);
 			while (resultSet.next()) {
-				System.out.println(resultSet.getString(id));
-				System.out.println(resultSet.getString(room_type));;
-				System.out.println(resultSet.getString(created_date));
-				System.out.println(resultSet.getString(updated_date));
-				System.out.println(resultSet.getString(is_active));
+//				System.out.println(resultSet.getString(id));
+//				System.out.println(resultSet.getString(room_type));;
+//				System.out.println(resultSet.getString(created_date));
+//				System.out.println(resultSet.getString(updated_date));
+//				System.out.println(resultSet.getString(is_active));
 			}
+		
 			con.close();
 		} catch (Exception ex) {
 			System.err.println(ex);
 		}
+		
 
 	}
 
